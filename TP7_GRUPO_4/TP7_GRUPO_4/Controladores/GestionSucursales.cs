7fcd1bed7fc02c110e0dc306c68bc51ee9ce6fb3
user52689 <lalo2105@proton.me>
2024-10-14 09:45:13 -0300

    version mas actual a master

---
+++ TP7_GRUPO_4/TP7_GRUPO_4/Controladores/GestionSucursales.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace TP7_GRUPO_4.Controladores
+{
+    public class GestionSucursales
+    {
+        private DataTable CrearDataTableSucursales()
+        {
+            DataTable sucursalesSeleccionadas = new DataTable();
+            sucursalesSeleccionadas.Columns.Add("SucursalID", typeof(string));
+            sucursalesSeleccionadas.Columns.Add("NombreSucursal", typeof(string));
+            sucursalesSeleccionadas.Columns.Add("DescripcionSucursal", typeof(string));
+
+            return sucursalesSeleccionadas;
+        }
+
+        public void VariableSessionSucursalesSeleccionadas(object sender, CommandEventArgs e, Label lblSucursalSeleccionada, Label lblCantidadSucursalesSeleccionadas)
+        {
+            if (e.CommandArgument == null) return;
+
+            string[] sucursal = e.CommandArgument.ToString().Split(',');
+            string id = sucursal[0];
+            string nombre = sucursal[1];
+            string descp = sucursal[2];
+
+            DataTable sucursalesSeleccionadas = HttpContext.Current.Session["SucursalesSeleccionadas"] as DataTable;
+
+            if (sucursalesSeleccionadas == null)
+            {
+                sucursalesSeleccionadas = CrearDataTableSucursales();
+            }
+
+            if (!sucursalesSeleccionadas.AsEnumerable().Any(row => row.Field<string>("NombreSucursal") == nombre))
+            {
+                DataRow dr = sucursalesSeleccionadas.NewRow();
+                dr["SucursalID"] = id;
+                dr["NombreSucursal"] = nombre;
+                dr["DescripcionSucursal"] = descp;
+                sucursalesSeleccionadas.Rows.Add(dr);
+
+                HttpContext.Current.Session["SucursalesSeleccionadas"] = sucursalesSeleccionadas;
+
+                if (lblSucursalSeleccionada != null)
+                {
+                    lblSucursalSeleccionada.Text = "Sucursal seleccionada: " + sucursal[1];
+                    lblCantidadSucursalesSeleccionadas.Text = "Total de sucursales seleccionadas: " + sucursalesSeleccionadas.Rows.Count;
+                }
+            }
+            else
+            {
+                if (lblSucursalSeleccionada != null)
+                {
+                    lblSucursalSeleccionada.Text = "La sucursal ya ha sido seleccionada: " + sucursal[1];
+                }
+            }
+        }
+
+        public void CargarSucursalesEnGridView(GridView grdSucursales)
+        {
+            DataTable sucursalesSeleccionadas = HttpContext.Current.Session["SucursalesSeleccionadas"] as DataTable;
+
+            if (sucursalesSeleccionadas != null && sucursalesSeleccionadas.Rows.Count > 0)
+            {
+                grdSucursales.DataSource = sucursalesSeleccionadas;
+                grdSucursales.DataBind();
+            }
+        }
+    }
+}
---
+++ TP7_GRUPO_4/TP7_GRUPO_4/Vistas/ListadoSucursalesSeleccionadas.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP7_GRUPO_4.Vistas
+{
+
+
+    public partial class SeleccionarSucursales
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control hlSeleccionSucursales.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlSeleccionSucursales;
+
+        /// <summary>
+        /// Control hlListadoSucursalesSeleccionadas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlListadoSucursalesSeleccionadas;
+
+        /// <summary>
+        /// Control lblListadoSucursales.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblListadoSucursales;
+
+        /// <summary>
+        /// Control lblBusquePorNombreSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBusquePorNombreSucursal;
+
+        /// <summary>
+        /// Control txtBusquedaPorNombreSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBusquedaPorNombreSucursal;
+
+        /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscar;
+
+        /// <summary>
+        /// Control DataList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList1;
+
+        /// <summary>
+        /// Control SqlDataSource2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
+
+        /// <summary>
+        /// Control ListView1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView ListView1;
+
+        /// <summary>
+        /// Control SqlDataSource1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// Control lblMostrarSucursalesSeleccionadas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMostrarSucursalesSeleccionadas;
+
+        /// <summary>
+        /// Control lblCantidadSucursalesSeleccionadas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCantidadSucursalesSeleccionadas;
+    }
+}
