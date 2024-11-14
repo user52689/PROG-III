3be94f1d4924967f1f2d13f91212e402b104539a
Lisandro <98889906+xxLich@users.noreply.github.com>
2024-11-13 21:54:58 -0300

    Update NegociosMedico.cs

--- TP Integrador/Negocios/NegociosMedico.cs
+++ TP Integrador/Negocios/NegociosMedico.cs
@@ -48,31 +48,16 @@ namespace Negocios
         }
 
         //---------------------------------------------------------------------------------------
-        public Medico ObtenerMedicosPorLegajo(int Legajo)
-        {
-            DataTable dt = accesoDatos.FiltrarMedicoPorLegajo(Legajo);
-            if (dt.Rows.Count > 0)
-            {
-                DataRow row = dt.Rows[0];
-                return new Medico
-                {
-                    Legajo = Convert.ToInt32(row["Legajo_med"]),
-                    DNI = row["DNI_med"].ToString(),
-                    Nombre = row["Nombre_med"].ToString(),
-                    Apellido = row["Apellido_med"].ToString(),
-                    Genero = Convert.ToInt32(row["Genero_med"]),
-                    Nacionalidad = Convert.ToInt32(row["Nacionalidad_med"]),
-                    FechaNacimiento = Convert.ToDateTime(row["FechaNacimiento_med"]),
-                    Direccion = row["Direccion_med"].ToString(),
-                    Localidad = Convert.ToInt32(row["Localidad_med"]),
-                    Provincia = Convert.ToInt32(row["Provincia_med"]),
-                    CorreoElectronico = row["CorreoElectronico_med"].ToString(),
-                    Telefono = row["Telefono_med"].ToString(),
-                    Especialidad = Convert.ToInt32(row["Especialidad_med"])
-                };
-            }
-            return null;
-        }
+         public List<Medico> obtenerListaMedicos()
+   {
+       return datosMedico.ObtenerListaMedicos();
+   }
+
+
+   public List<Medico> ObtenerMedicosPorLegajo(string legajo)
+   {
+       return datosMedico.FiltrarMedicosPorLegajo(legajo);
+   }
 
         public DataTable ModificarMedicoNegocio(Medico medico)
         {
