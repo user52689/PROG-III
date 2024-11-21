fb23a4c4a22aba3db6e96cb3ab14c79c7d3ac3a9
Lisandro <98889906+xxLich@users.noreply.github.com>
2024-11-21 15:36:39 -0300

    Update ListadoPaciente.aspx.cs
    validaciones + corrección de conexión a la bbd

--- TP Integrador/Vistas/Usuarios/Administrador/ListadoPaciente.aspx.cs
+++ TP Integrador/Vistas/Usuarios/Administrador/ListadoPaciente.aspx.cs
@@ -13,21 +13,58 @@ namespace Vistas
     {
         private NegociosPaciente negociosPaciente = new NegociosPaciente();
 
-     
-
         void mostrarTodos()
         {
             var listaPacientes = negociosPaciente.ObtenerTodosLosPacientes();
             grdListadoPaciente.DataSource = listaPacientes;
             grdListadoPaciente.DataBind();
+
+           
+            lblMensajePaciente.Visible = false;
+            grdListadoPaciente.Visible = true; 
         }
 
         void mostrarDniPaciente()
         {
-            string dni = txtBuscarPorDNI.Text;
+            string dni = txtBuscarPorDNI.Text.Trim();
+
+            
+            if (string.IsNullOrEmpty(dni))
+            {
+                lblMensajePaciente.Text = "Por favor, ingrese un DNI.";
+                lblMensajePaciente.ForeColor = System.Drawing.Color.Red;
+                lblMensajePaciente.Visible = true; 
+                grdListadoPaciente.Visible = false; 
+                return;
+            }
+
+            
+            if (dni.Length > 11)
+            {
+                lblMensajePaciente.Text = "DNI NO VALIDO.";
+                lblMensajePaciente.ForeColor = System.Drawing.Color.Red;
+                lblMensajePaciente.Visible = true; 
+                grdListadoPaciente.Visible = false; 
+                return;
+            }
+
             var listaPacientes = negociosPaciente.ObtenerPacientesPorDNI(dni);
             grdListadoPaciente.DataSource = listaPacientes;
             grdListadoPaciente.DataBind();
+
+           
+            if (listaPacientes.Count == 0)
+            {
+                lblMensajePaciente.Text = "No se encontró un paciente con el DNI especificado.";
+                lblMensajePaciente.ForeColor = System.Drawing.Color.Red;
+                lblMensajePaciente.Visible = true; 
+                grdListadoPaciente.Visible = false;
+            }
+            else
+            {
+                lblMensajePaciente.Visible = false;  
+                grdListadoPaciente.Visible = true; 
+            }
         }
 
         protected void btnBuscarPaciente_Click(object sender, EventArgs e)
@@ -46,5 +83,5 @@ namespace Vistas
             Response.Redirect("InicioSesion.aspx");
         }
     }
-
 }
+
