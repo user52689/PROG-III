6f162267cc9f881526d83894dd6dae9bfa0bc1c6
usuario52689 <lalo210511@gmail.com>
2024-11-29 21:35:47 -0300

    Informes Completos

---
+++ TP Integrador/Vistas/Controlador/DropDownListControlador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Vistas.Helper
+{
+    public class DropDownListControlador
+    {
+        public static void CargarDDL(DropDownList ddl, IEnumerable lista, string Descripcion, string valor)
+        {
+            ddl.DataSource = lista;
+            ddl.DataTextField = Descripcion;
+            ddl.DataValueField = valor;
+            ddl.DataBind();
+            ddl.Items.Insert(0, new ListItem("--Seleccione--", "0"));
+        }
+    }
+}
